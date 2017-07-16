80f1c1e5a2010ed2b78a285f741b74fd01a4f05c
luckygeo <tellmegao@gmail.com>
2017-07-17 07:05:14 +0800

    docs(class): add docs

--- common-classes-use/src/main/java/me/common/utils/NumUtils.java
+++ common-classes-use/src/main/java/me/common/utils/NumUtils.java
@@ -3,13 +3,21 @@ package me.common.utils;
 import java.text.DecimalFormat;
 
 /**
- * Created by tgs on 2017/7/14 15:48.
- *
- * 数字类格式化类
- *
- * @星期五
+ * Created
+ * 数字类工具
  */
 public class NumUtils {
+
+    /**
+     * 格式化float为2位小数
+     *
+     * @param num
+     *        需要格式的数字
+     *
+     * @return float
+     *         返回格式后的数字
+     *
+     */
     public static float getDouble(float num) {
         DecimalFormat decimalFormat = new DecimalFormat(".00");
         return Float.parseFloat(decimalFormat.format(num));
